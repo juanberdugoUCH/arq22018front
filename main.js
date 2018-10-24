bdf1580b79bac4cbc0d670081f1e0482d79373c5
AilinCarribero <42686226+AilinCarribero@users.noreply.github.com>
2018-10-24 19:50:21 -0300

    24-10 cambios en signos mas y menos

--- main.js
+++ main.js
@@ -15,28 +15,6 @@ $(function() {
     function cargarPuntos(){
         var puntosServices = new PuntosService();
         puntosServices.getPuntos().then(function(puntos) {
-<<<<<<< HEAD
-            console.log(puntos);
-            fila="";
-            puntos.forEach(function(punto) {
-                fila="<tr>";
-                fila += '<td rowspan="2"  class="align-middle">' + punto.nombre + '</td>';
-                fila += '<td rowspan="2"  class="align-middle">' + punto.cantidadDiscord + '</td>';
-                fila += '<td rowspan="2"  class="align-middle">' + punto.tpDiscord + '</td>';
-                fila += '<td rowspan="2"  class="align-middle">' + punto.cantidadCasaMaritima + '</td>';
-                fila += '<td rowspan="2"  class="align-middle">' + punto.tpCasaMaritima + '</td>';
-                fila += '<td rowspan="2"  class="align-middle">' + punto.cantidadMision + '</td>';
-                fila += '<td rowspan="2"  class="align-middle">' + punto.tpMision + '</td>';
-                fila += '<td rowspan="2"  class="align-middle">' + punto.cantidadRenovacionContrato + '</td>';
-                fila += '<td rowspan="2"  class="align-middle">' + punto.tpRenovacionContrato + '</td>';
-                fila += '<td rowspan="2"  class="align-middle">' + punto.cantidadAyudarMiembro + '</td>';
-                fila += '<td rowspan="2"  class="align-middle">' + punto.tpAyudarMiembro + '</td>';
-                fila += '<td rowspan="2"  class="align-middle">' + punto.tp + '</td>';
-                fila += '<td rowspan="2"  class="align-middle">' + punto.nivelBono + '</td>';
-                fila="</tr>";
-                
-                $tablaPuntos.append(fila);
-=======
             fila="";
             puntos.forEach(function(punto) {
                 console.log(punto);
@@ -82,7 +60,6 @@ $(function() {
                 
                 $tablaPuntos.append(fila);  
                 console.log(fila);
->>>>>>> 24-10 agregacion del signo mas y menos
             });
         });
     }
