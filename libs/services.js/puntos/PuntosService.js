979313e9c247b8d1b60bf5e342dc47834ba41064
Neski83 <eljuancho@gmail.com>
2018-10-24 20:57:57 -0300

    added points add/remove functionality

--- libs/services.js/puntos/PuntosService.js
+++ libs/services.js/puntos/PuntosService.js
@@ -3,9 +3,18 @@ class PuntosService {
     constructor() {
         //this.serviceUrl = "/api/puntos";
         this.serviceUrl = "./libs/services.js/puntos/puntosMock.json";
+        this.editPointsUrl = "/api/editPoints";
     }
 
     getPuntos(){
         return $.getJSON(this.serviceUrl);   
+    }
+
+    editarPuntos(operation, player, activity){
+        return $.post(this.editPointsUrl, {"operation":operation,
+                            "player": player,
+                            "activity": activity
+                         }
+                        );
     }
 }
